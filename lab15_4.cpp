b687f979ab475d0b5c30e4a05bdfbf3a8973c934
pubkung1ez <tirapan_30506@sysp.ac.th>
2026-01-30 16:35:50 +0700

    suss

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,16 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d){
+    int *p[4] = {&a,&b,&c,&d};
+    for(int i =3; i >0;i--){
+        int tew = rand()%i;
+        int temp = *p[i];
+        *p[i] = *p[tew];
+        *p[tew] = temp;
+    }
+}
+
+
+
+
